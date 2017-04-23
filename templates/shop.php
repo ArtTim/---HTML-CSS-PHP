1ef63a3c1ce56086ce0ea54ba9ba9db458fe99f7
Тимур Булатов < >
2017-04-23 04:12:33 +0300

    Продукты

---
+++ templates/shop.php
@@ -0,0 +1,24 @@
+    <h1>
+        Каталог товаров
+    </h1>
+    <?php
+    foreach ($goods as $good): ?>
+    <div>
+    <div class="shopUnit">
+        <img src="<?php echo $good['img']?>" />
+
+        <div class="shopUnitName">
+            <?php echo $good['name'];?>
+        </div>
+        <div class="shopUnitShortDesc">
+            <?php echo $good["desc"]; ?>
+        </div>
+        <div class="shopUnitPrice">
+            Цена: <?php echo $good['price']?>
+        </div>
+        <a href="index.php?page=product&id=<? echo $good['id'];?>" class="shopUnitMore">
+            Подробнее
+        </a>
+    </div>
+<?php endforeach;?>
+    </div>
