b65098e1791e1edd07ff2fcebaccf11f4cc49d10
Тимур Булатов < >
2017-04-23 04:12:26 +0300

    Описание продукта

---
+++ templates/openproduct.php
@@ -0,0 +1,12 @@
+<div id="openedProduct-img">
+    <img src="<? echo $good['img'];?>" alt=""></div>
+<div id="openedProduct-content">
+    <h1 id="openedProduct-name">
+        <? echo $good['name'];?>
+    </h1>
+    <div id="openedProduct-desc">
+        <? echo $good['desc']; ?>
+    </div>
+    <div id="openedProduct-price">
+        Цена: <? echo $good['price'];?>
+    </div>
